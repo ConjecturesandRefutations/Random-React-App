67bd02a096f516631620122923460cf351e5823d
Alfie <alfiecollins44@gmail.com>
2024-04-01 14:34:32 +0100

    stuff

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h2>Hello React!</h2>
+      <img src="https://i.imgur.com/8uPMJOs.png" className="App-logo" alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
